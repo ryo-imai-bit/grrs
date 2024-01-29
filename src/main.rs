d84b84aec1bc2f819f02714c0f03b396c60f5d9f
ryo-imai-bit <mayoimaimai9@gmail.com>
2024-01-30 01:30:28 +0900

    add buffering output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::io::{self, Write};
 use std::fs::File;
 use anyhow::{Context, Result};
 use clap::Parser;
@@ -16,10 +17,13 @@ fn main() -> Result<()> {
     let reader = BufReader::new(f);
     let content = reader.lines().map(|l| l.expect("could not parse line"));
 
+    let stdout = io::stdout();
+    let mut handle = io::BufWriter::new(stdout);
+
     for line in content {
         // 文字列リテラルや文字列は、一般的に文字列スライスとして関数に渡され、これによって、実際には所有権を渡す必要がないほとんど場合において柔軟性が増す。
         if line.contains(&args.pattern) {
-            println!("{}", line);
+            writeln!(handle, "{}", line)?;
         }
     }
 
