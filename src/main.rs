3b6fc16e8f87fb673426518e5d39f8715c951b93
ryo-imai-bit <mayoimaimai9@gmail.com>
2024-01-29 22:24:26 +0900

    impl buffer reader from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
 use clap::Parser;
 
 #[derive(Parser)]
@@ -8,9 +11,12 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let f = File::open(&args.path).unwrap();
+    let reader = BufReader::new(f);
+    // let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = reader.lines().map(|l| l.expect("could not parse line"));
 
-    for line in content.lines() {
+    for line in content {
         // 文字列リテラルや文字列は、一般的に文字列スライスとして関数に渡され、これによって、実際には所有権を渡す必要がないほとんど場合において柔軟性が増す。
         if line.contains(&args.pattern) {
             println!("{}", line);
