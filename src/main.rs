90a98fb2b19edc6ff9a5a3a332ae73838e6b4dd6
ryo-imai-bit <mayoimaimai9@gmail.com>
2024-01-29 22:05:49 +0900

    impl tmp grrs

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    for line in content.lines() {
+        // 文字列リテラルや文字列は、一般的に文字列スライスとして関数に渡され、これによって、実際には所有権を渡す必要がないほとんど場合において柔軟性が増す。
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
